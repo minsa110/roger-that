ca54946d10096b61d11ed49ca61e644eb15430ea
Soojin Min <minsa110@uw.edu>
2017-05-19 15:21:22 -0700

    working on scrollable / nav bar

--- js/main.js
+++ js/main.js
@@ -7,32 +7,29 @@ var data = [{
     value: 34
 }];
 
-var treeData = [
-  {
-    "name": "United States of America",
-    "parent": "null",
-    "children": [
-      {
-        "name": "States",
-        "parent": "United States of America",
-        "children": [
-          {
-            "name": "Counties",
-            "parent": "States"
-            
-          }]
-      },
-      {
-        "name": "Zip Codes (ZCTA)",
-        "parent": "United States of America"
-      },
-      {
-        "name": "114th CD",
-        "parent": "United States of America"
-      }
-    ]
-  }
-];
+// var treeData = [{
+//     "name": "Landing",
+//     "children": [{
+//         "name": "At a Glance",
+//         "parent": "Landing",
+//         "children": [{
+//             "name": "Count of Incidents",
+//             "parent": "At a Glance",
+//             "children": [{
+//                 "name": "Highest Ransom",
+//                 "parent": "Count of Incidents",
+//                 "children": [{
+//                     "name": "Cause and Frequency of Attacks",
+//                     "parent": "Highest Ransom",
+//                     "children": [{
+//                         "name": "Common Methods of Claims",
+//                         "parent": "Cause and Frequency of Attacks"}]
+//                     }]
+//                 }]
+//             }]
+//         }]
+//     }
+// ];
 
 $(function() {
     // Instantiate your chart with given settings
@@ -68,6 +65,212 @@ $(function() {
         chart.datum(data).call(myChart);
     };
 
+///////////////////
+
+var treeData = {
+    "name": "Landing",
+    "children": [{ 
+        "name": "At a Glance",
+        "children": [{
+            "name": "Count of Incidents",
+            "children": [{
+                "name": "Highest Ransom",
+                "children": [{
+                    "name": "Cause and Frequency of Attacks",
+                    "children": [{
+                        "name": "Common Method of Claims"
+                    }]
+                }]
+            }]
+        }]
+    }]
+};
+
+// Set the dimensions and margins of the diagram
+var margin = {top: 20, right: 90, bottom: 30, left: 90},
+    width = 1200 - margin.left - margin.right,
+    height = 1200 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+// appends a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.right + margin.left)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate("
+          + margin.left + "," + margin.top + ")");
+
+var i = 0,
+    duration = 750,
+    root;
+
+// declares a tree layout and assigns the size
+var treemap = d3.tree().size([height, width]);
+
+// Assigns parent, children, height, depth
+root = d3.hierarchy(treeData, function(d) { return d.children; });
+root.x0 = height / 2;
+root.y0 = 0;
+
+
+// Collapse after the second level
+// root.children.forEach(collapse);
+
+updateTree(root);
+
+// Collapse the node and all it's children
+// function collapse(d) {
+//   if(d.children) {
+//     d._children = d.children
+//     d._children.forEach(collapse)
+//     d.children = null
+//   }
+// }
+
+function updateTree(source) {
+
+  // Assigns the x and y position for the nodes
+  var treeData = treemap(root);
+
+  // Compute the new tree layout.
+  var nodes = treeData.descendants(),
+      links = treeData.descendants().slice(1);
+
+  // Normalize for fixed-depth.
+  nodes.forEach(function(d){ d.y = d.depth * 100});
+
+  // ****************** Nodes section ***************************
+
+  // Update the nodes...
+  var node = svg.selectAll('g.node')
+      .data(nodes, function(d) {return d.id || (d.id = ++i); });
+
+  // Enter any new modes at the parent's previous position.
+  var nodeEnter = node.enter().append('g')
+      .attr('class', 'node')
+      .attr("transform", function(d) {
+        // return "translate(" + source.y0 + "," + source.x0 + ")";
+        return "translate(" + d.x + "," + d.y + ")";
+    });
+    // .on('click', click);
+
+  // Add Circle for the nodes
+  nodeEnter.append('circle')
+      .attr('class', 'node')
+      .attr('r', 1e-6)
+      .style("fill", function(d) {
+          return d._children ? "lightsteelblue" : "#fff";
+      });
+
+  // Add labels for the nodes
+  nodeEnter.append('text')
+      .attr("dy", ".35em")
+      .attr("x", function(d) {
+          return d.children || d._children ? -13 : 13;
+      })
+      .attr("text-anchor", function(d) {
+          return d.children || d._children ? "end" : "start";
+      })
+      .text(function(d) { return d.data.name; });
+
+  // UPDATE
+  var nodeUpdate = nodeEnter.merge(node);
+
+  // Transition to the proper position for the node
+  nodeUpdate.transition()
+    .duration(duration)
+    .attr("transform", function(d) { 
+        // return "translate(" + d.y + "," + d.x + ")";
+        return "translate(" + d.x + "," + d.y + ")";
+     });
+
+  // Update the node attributes and style
+  nodeUpdate.select('circle.node')
+    .attr('r', 10)
+    .style("fill", function(d) {
+        return d._children ? "lightsteelblue" : "#fff";
+    })
+    .attr('cursor', 'pointer');
+
+
+  // Remove any exiting nodes
+  var nodeExit = node.exit().transition()
+      .duration(duration)
+      .attr("transform", function(d) {
+          return "translate(" + source.y + "," + source.x + ")";
+      })
+      .remove();
+
+  // On exit reduce the node circles size to 0
+  nodeExit.select('circle')
+    .attr('r', 1e-6);
+
+  // On exit reduce the opacity of text labels
+  nodeExit.select('text')
+    .style('fill-opacity', 1e-6);
+
+  // ****************** links section ***************************
+
+  // Update the links...
+  var link = svg.selectAll('path.link')
+      .data(links, function(d) { return d.id; });
+
+  // Enter any new links at the parent's previous position.
+  var linkEnter = link.enter().insert('path', "g")
+      .attr("class", "link")
+      .attr('d', function(d){
+        var o = {x: source.x0, y: source.y0}
+        return diagonal(o, o)
+      });
+
+  // UPDATE
+  var linkUpdate = linkEnter.merge(link);
+
+  // Transition back to the parent element position
+  linkUpdate.transition()
+      .duration(duration)
+      .attr('d', function(d){ return diagonal(d, d.parent) });
+
+  // Remove any exiting links
+  var linkExit = link.exit().transition()
+      .duration(duration)
+      .attr('d', function(d) {
+        var o = {x: source.x, y: source.y}
+        return diagonal(o, o)
+      })
+      .remove();
+
+  // Store the old positions for transition.
+  nodes.forEach(function(d){
+    d.x0 = d.x;
+    d.y0 = d.y;
+  });
+
+  // Creates a curved (diagonal) path from parent to the child nodes
+  function diagonal(s, d) {
+    path = `M ${s.x} ${s.y}
+        C ${(s.x + d.x) / 2} ${s.y},
+            ${(s.x + d.x) / 2} ${d.y},
+            ${d.x} ${d.y}`
+
+    return path
+  }
+
+//   // Toggle children on click.
+//   function click(d) {
+//     if (d.children) {
+//         d._children = d.children;
+//         d.children = null;
+//       } else {
+//         d.children = d._children;
+//         d._children = null;
+//       }
+//     updateTree(d);
+//   }
+}
+
+//////////////////
 
     // Define a new scroller, and use the `.container` method to specify the desired container
     var scroll = scroller()
@@ -78,7 +281,6 @@ $(function() {
 
     // Specify the function you wish to activate when a section becomes active
     scroll.on('active', function(index) {
-        console.log(index)
         update(index);
     })
 });
